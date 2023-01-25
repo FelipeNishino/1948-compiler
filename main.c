558b6f532ce6d1ecd8cd05bee163ddd84d6a91b1
FelipeNishino <49385633+FelipeNishino@users.noreply.github.com>
2023-01-25 18:14:29 -0300

    Add delete register function

--- main.c
+++ main.c
@@ -96,6 +96,43 @@ RegisterNode *find(char *key) {
     return current;
 }
 
+RegisterNode *delete(char key) {
+
+    //start from the first link
+    RegisterNode *current = head;
+    RegisterNode *previous = NULL;
+
+    //if list is empty
+    if(head == NULL) {
+        return NULL;
+    }
+
+    //navigate through list
+    while(strcmp(current->reg.name, key) != 0) {
+
+        //if it is last node
+        if(current->next == NULL) {
+            return NULL;
+        } else {
+            //store reference to current link
+            previous = current;
+            //move to next link
+            current = current->next;
+        }
+    }
+
+    //found a match, update the link
+    if(current == head) {
+        //change first to point to next link
+        head = head->next;
+    } else {
+        //bypass the current link
+        previous->next = current->next;
+    }
+
+    return current;
+}
+
 Instruction extractInstructionData(char *codeline) {
     int i = 0, step = 0;
     char *token;
